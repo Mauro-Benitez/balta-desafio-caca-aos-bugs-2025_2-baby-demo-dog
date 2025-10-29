6daa8197d4ab00cbe66cc2441721d31b9e62a0c1
mauro-benitez <mauro.cbenitez@gmail.com>
2025-10-28 22:31:54 -0300

    Configurado o EF e realizado a primeira migrations

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,12 @@
+using BugStore.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var conectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(Options => Options.UseNpgsql(conectionString));
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
